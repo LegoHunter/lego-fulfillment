09e6dec2737ca6b046d721cb86e2bc21ff3deee5
tvatt <github@vattima.com>
2025-02-26 21:16:49 -0500

    Initial commit for lego-fulfillment

---
+++ src/test/java/net/legohunter/fulfillment/LegoFulfillmentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.legohunter.fulfillment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoFulfillmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
